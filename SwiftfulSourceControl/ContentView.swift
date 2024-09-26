9893fad730b43336397a44a6c1911aaf69b239ae
Sahas Pasya <pasyasahas17@gmail.com>
2024-09-26 13:28:38 -0500

    [Feature] Rename Subscribe now button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful!")
-            Button("Subscribe now!") {
+            Button("Subscribe") {
                 
             }
             
