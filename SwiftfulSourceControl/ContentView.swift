b57a09cb35e9d2f6bed61efb103ca123d7d1c62d
Sahas Pasya <pasyasahas17@gmail.com>
2024-09-26 12:12:20 -0500

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Pasya Sahas on 9/26/24.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
+
+
 import SwiftUI
 
 struct ContentView: View {
