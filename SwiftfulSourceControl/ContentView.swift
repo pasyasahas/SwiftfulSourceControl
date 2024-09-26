2c7a9a3f68eecd7e63c193ff5221895942d8da6b
Sahas Pasya <pasyasahas17@gmail.com>
2024-09-26 13:11:20 -0500

    [Feature] Update Subscribe Text to Subscribe now! (was subscribe)

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful!")
-            Button("Subscribe") {
+            Button("Subscribe now!") {
                 
             }
             
