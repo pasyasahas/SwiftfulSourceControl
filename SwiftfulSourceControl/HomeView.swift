10d65a1498982a3151c38cbefdc7871f511a001d
Sahas Pasya <pasyasahas17@gmail.com>
2024-09-26 11:59:11 -0500

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Pasya Sahas on 9/26/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Second Screen")
+    }
+}
+
+#Preview {
+    HomeView()
+}
