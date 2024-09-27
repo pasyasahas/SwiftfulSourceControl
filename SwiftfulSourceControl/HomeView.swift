829fd8e2f7fd17bc177c7ffa3ad259f0debbfb6c
Sahas Pasya <pasyasahas17@gmail.com>
2024-09-27 13:19:15 -0500

    [Feature] Update title

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var title: String = "Hello, Sahas!"
+    @State private var title: String = "Hello, World!"
     
     var body: some View {
         
