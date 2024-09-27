6cea156843800f74b5a5182a89b424efcab33325
Sahas Pasya <pasyasahas17@gmail.com>
2024-09-27 13:14:09 -0500

    [Clean] Update documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -12,6 +12,9 @@
  Stash = Save changes for later
  Push = Send local commits to remote repo
  Pull = Fetch remote commits to local repo
+ Merge = Joining two different branch
+ Rebase = Moving one branch on top of another branch
+ Cherry picking = Duplicating (copying) one commit from one branch to another
  
  COMMIT MESSAGES
  
