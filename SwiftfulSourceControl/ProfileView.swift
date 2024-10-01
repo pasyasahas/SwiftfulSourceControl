289552a57bd3a34760789c430ab364d2c723de2e
Sahas Pasya <pasyasahas17@gmail.com>
2024-10-01 12:08:05 -0500

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Pasya Sahas on 10/1/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Sahas!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
